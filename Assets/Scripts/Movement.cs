cbd6c9d6f78aabafa7c005e84b57588c0f4ec142
Eduard Beukes <ed@cingaphambili.co.za>
2024-02-08 14:10:57 +0200

    Implemented movement for Pacman.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,76 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private float speed = 8.0f;
+    [SerializeField] private float speedMultiplier = 1.0f;
+    [SerializeField] private Vector2 initialDirection;
+    [SerializeField] private LayerMask obstacleLayer;
+    [SerializeField] private float footprint;
+
+    public new Rigidbody2D rigidbody { get; private set; }
+    public Vector2 direction { get; private set; }
+    public Vector2 nextDirection { get; private set; }
+    public Vector3 startingPosition { get; private set; }
+
+
+
+    public void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        startingPosition = transform.position;
+    }
+
+    public void Start()
+    {
+        ResetState();
+    }
+
+    public void Update()
+    {
+        if (nextDirection != Vector2.zero)
+        {
+            SetDirection(nextDirection);
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        var position = this.rigidbody.position;
+        var translation = speedMultiplier * speed * Time.fixedDeltaTime * direction;
+        
+        rigidbody.MovePosition(position + translation);
+    }
+
+    public void ResetState()
+    {
+        speedMultiplier = 1.0f;
+        direction = initialDirection;
+        nextDirection = Vector2.zero;
+        transform.position = startingPosition;
+        rigidbody.isKinematic = false;
+
+        this.enabled = true;
+    }
+
+    public void SetDirection(Vector2 direction, bool forced = false)
+    {
+        if (forced || !Occupied(direction))
+        {
+            this.direction = direction;
+            nextDirection = Vector2.zero;
+        }
+        else
+        {
+            nextDirection = direction;
+        }
+    }
+
+    public bool Occupied(Vector2 direction)
+    {
+        var hit = Physics2D.BoxCast(transform.position, Vector2.zero * footprint, 0f, direction, 1.5f,obstacleLayer);
+        return hit.collider != null;
+    }
+}
--- Assets/Scripts/Pacman.cs
+++ Assets/Scripts/Pacman.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Movement))]
 public class Pacman : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Movement movementController;
+
+    public void Awake()
     {
-        
+        movementController = GetComponent<Movement>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            movementController.SetDirection(Vector2.up);
+        }
         
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            movementController.SetDirection(Vector2.down);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            movementController.SetDirection(Vector2.left);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            movementController.SetDirection(Vector2.right);
+        }
+
+        float angle = Mathf.Atan2(this.movementController.direction.y, -movementController.direction.x);
+        transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, -Vector3.forward);
     }
 }
